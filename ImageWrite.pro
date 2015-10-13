2c255b4201ca39a40cbfb9d8815f33c73492ded2
jmacey <jmacey@bournemouth.ac.uk>
2015-10-13 11:58:42 +0100

    basic class working

---
+++ ImageWrite.pro
@@ -0,0 +1,17 @@
+# Target is the name of the exe
+TARGET=ImageWrite
+# where to put .o files $$PWD means in proj dir
+OBJECTS_DIR=$$PWD/obj
+# now set some QMAKE settings
+# don't build against qt libs
+CONFIG-=qt
+#use C++ 11
+CONFIG+=c++11
+#DEFINES+=NDEBUG
+# add source files
+SOURCES +=$$PWD/src/main.cpp \
+          $$PWD/src/ImageWrite.cpp
+
+HEADERS +=$$PWD/include/ImageWrite.h
+
+INCLUDEPATH+= $$PWD/include
---
+++ include/ImageWrite.h
@@ -0,0 +1,21 @@
+#ifndef IMAGEWRITE_H__
+#define IMAGEWRITE_H__
+
+#include <string>
+
+class ImageWrite
+{
+  public :
+    ImageWrite() : m_width(0), m_height(0), m_data(0){}
+    ImageWrite(int _w, int _h);
+    ~ImageWrite();
+    void save(const std::string &_fname);
+    void setPixel(int _x, int _y, unsigned char _r,
+                  unsigned char _g, unsigned char _b);
+  private :
+    int m_width;
+    int m_height;
+    unsigned char *m_data;
+};
+
+#endif
